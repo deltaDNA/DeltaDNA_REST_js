f97630683418b9dc080e33e8900923e4109c3f92
Steven <van.stiphout.steven@gmail.com>
2016-03-01 15:52:13 +0000

    added url config, prevent event sending when there are no events, and removed SDK naming since this is not an sdk

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,7 @@
-// Settings
-var url = 'http://127.0.0.1:8080'; //"http://collect4792jmprb.deltadna.net/collect/api/22079697190426055695055037414340";
+// Setting the URL
+var envKey = '22079697190426055695055037414340'
+var collectURL = 'http://collect4792jmprb.deltadna.net/collect/api/'
+var url = 'http://127.0.0.1:8080'+''; collectURL+envKey+'/bulk'
 var eventList = [];
 
 // Helper functions
@@ -13,15 +15,19 @@ function generateUUID() {
 }
 
 var sendEventList = _.debounce (function() {
-  var trackingEvents = $.extend([], eventList);
+  if (eventList.length ==0){
+    console.log('No events to send');
+    return;
+  }
+  var recordedEvents = $.extend([], eventList);
   eventList = [];
 
-  console.log('Sending events: ', trackingEvents);
+  console.log('Sending events: ', recordedEvents);
 
   $.ajax({
     type: 'POST',
     url: url,
-    data: JSON.stringify({ eventList: trackingEvents}),
+    data: JSON.stringify({ eventList: recordedEvents}),
     success: function() {
       console.log('success');
     },
@@ -29,7 +35,7 @@ var sendEventList = _.debounce (function() {
       console.log('error', arguments)
     }
   });
-}, 300);
+}, 500);
 
 function getDefaults () {
   return {
@@ -41,27 +47,26 @@ function getDefaults () {
   };
 }
 
-function trackEvent (event) {
+function recordEvent (event) {
   event = $.extend(true, getDefaults(), event);
   eventList.push(event);
 
-  console.log('Tracking event ' + event.eventName);
+  console.log('Recording event ' + event.eventName);
   console.log(event);
-  sendEventList();
 }
 
 function getUser () {
+  //use a local var that retrieves from the localStorage to not have the variable living in two places
   var userID = localStorage.getItem('userID');
 
   if (userID === null) {
-    console.log('new user generated');
+    console.log('New user generated');
     userID = generateUUID();
     localStorage.setItem('userID', userID);
-    trackEvent({
+    recordEvent({
       eventName: 'newPlayer'
     });
   }
-
   return userID;
 }
 
@@ -69,15 +74,15 @@ function getSession () {
   var sessionID = sessionStorage.getItem('sessionID');
 
   if (sessionID === null) {
-    console.log('new session generated');
+    console.log('New session generated');
     sessionID = generateUUID();
     sessionStorage.setItem('sessionID', sessionID);
 
-    // Send clientDevice and gameStarted event
-    trackEvent({
+    // Send clientDevice and gameStarted event since we start a new session
+    recordEvent({
       eventName: 'clientDevice'
     });
-    trackEvent({
+    recordEvent({
       eventName: 'gameStarted'
     });
   }
@@ -86,39 +91,41 @@ function getSession () {
 }
 
 function startDDNA () {
+
   var userID = getUser();
   console.log('UserID: ' + userID);
 
   var sessionID = getSession();
   console.log('sessionID: ' + sessionID);
+  sendEventList();
 }
 
 $(document).ready(function() {
-  //localStorage.setItem('userID', userID);
-  //sessionStorage.setItem('sessionID', sessionID);
+
+  $('#StartDDNA').click(function() {
+    startDDNA();
+  });
+
   $('#StopSDK').click(function() {
     sessionStorage.clear();
-    console.log('sessionStorage cleared');
+    console.log('sessionStorage cleared - sessionID deleted');
   });
 
   $('#newUser').click(function() {
     localStorage.clear();
-    console.log('localStorage cleared');
+    console.log('localStorage cleared - userID deleted');
   });
 
 
-  $('#StartSDK').click(function() {
-    startDDNA();
-  });
 
   $('#SendSimpleEvent').click(function() {
     var event = {
       eventName: 'simpleEvent',
-      eventsParams: {
+      eventParams: {
         action: 'open',
         option: 'menu'
       }
     };
-    trackEvent(event);
+    recordEvent(event);
   });
 });
